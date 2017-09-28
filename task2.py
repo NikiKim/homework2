4cd7771b642889fabb256d06438bc7b32ebc4d29
NikiKim <ridiska72@gmail.com>
2017-09-28 18:11:48 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,3 +6,7 @@ for i in range(len(array) - 1):
                 min_element = j
         array[min_element], array[i] = array[i], array[min_element]
         print(array[i])
+        
+# Сложность n*n, т.е. O(n^2)
+# Идет поиск наименьшего элемента и меняется с первым
+# затем ищется следующий наименьший и меняем со вторым 
